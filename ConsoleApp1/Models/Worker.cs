986c70b91448c9cefa9d7eee990a8f4b4a2e2939
Lucas Joaquim da Silva <lucasjoaquim147@gmail.com>
2020-02-05 09:11:30 -0300

    Class 119 - Enumeration part(2/3)

---
+++ ConsoleApp1/Models/Worker.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CourseApp.Models.Enums;
+
+namespace CourseApp.Models
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> contracts { get; set; } = new List<HourContract>();
+
+        public Worker()
+        {
+        }
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+        
+        public void addContract(HourContract contract)
+        {
+            contracts.Add(contract);
+        }
+
+        public void removeContract(HourContract contract)
+        {
+            contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach (HourContract contract in contracts)
+            {
+                if (contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.totalValue();
+                }
+            }
+            return sum;
+        }
+
+
+    }
+}
